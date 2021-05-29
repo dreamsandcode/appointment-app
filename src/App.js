9247056a0eae4ff1d757be8170c938ee7f24e35d
Brian Palmer <dreamsandcode@gmail.com>
2021-05-29 07:12:16 -0400

    initial commit, package cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import {FcAssistant,FcBinoculars} from 'react-icons/fc'
 
 function App() {
   return (
     <div className="App">
-     
+      <h1> <FcAssistant/> Appointment App</h1>
     </div>
   );
 }
